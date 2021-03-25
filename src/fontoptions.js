f0dcd7d26bb2daf3b3b20dfe78b2eab69fc0ba59
Stephen Kelman <stephenrkelman@gmail.com>
2021-03-24 17:29:43 -0700

    font stuff works

--- src/fontoptions.js
+++ src/fontoptions.js
@@ -1,10 +1,10 @@
 let fontFamilySegment = document.getElementById("fontFamilyButtonDiv");
 let selectedFontFamily = "currentFontFamily";
-const presetButtonFontFamilies = ["courier new", "arial", "times new roman", "impact"]
+const presetButtonFontFamilies = ["courier new", "arial", "times new roman", "impact"];
 
 // Reacts to a button click by marking the selected button and saving
 // the selection
-function handleButtonClick(event) {
+function handleFontClick(event) {
   // Remove styling from the previously selected font family
   let currentFontFamily = event.target.parentElement.querySelector(
     `.${selectedFontFamily}`
@@ -19,29 +19,28 @@ function handleButtonClick(event) {
   chrome.storage.sync.set({ fontFamily });
 }
 
-// Add a button to the segment for each supplied font family
+// Add a button to the fontFamilySegment for each supplied font family
 function constructOptions(buttonFontFamilies) {
   chrome.storage.sync.get("fontFamily", (data) => {
     let currentFontFamilySelection = data.fontFamily;
     // For each font family we were provided…
     for (let buttonFontFamily of buttonFontFamilies) {
-      // …create a button with that font family
+      // …create a button with that fontFamily
       let button = document.createElement("button");
       button.dataset.fontFamily = buttonFontFamily;
       button.style.fontFamily = buttonFontFamily;
-      button.innerHTML = "text";
-
-      // …mark the currentFontFamily selected font family
+      button.innerText = "text";
+      // …mark the currently selected font family
       if (buttonFontFamily === currentFontFamilySelection) {
         button.classList.add(selectedFontFamily);
       }
 
       // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
+      button.addEventListener("click", handleFontClick);
       fontFamilySegment.appendChild(button);
     }
   });
 }
 
-// Initialize the segment by constructing the color options
+// Initialize the fontFamilySegment by constructing the fontFamily options
 constructOptions(presetButtonFontFamilies);
--- src/options.js
+++ src/options.js
@@ -1,25 +1,25 @@
 let colorSegment = document.getElementById("colorButtonDiv");
-let selectedClassName = "currentColor";
+let selectedColor = "currentColor";
 const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
 // Reacts to a button click by marking the selected button and saving
 // the selection
-function handleButtonClick(event) {
+function handleColorClick(event) {
   // Remove styling from the previously selected color
   let currentColor = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
+    `.${selectedColor}`
   );
   if (currentColor && currentColor !== event.target) {
-    currentColor.classList.remove(selectedClassName);
+    currentColor.classList.remove(selectedColor);
   }
 
   // Mark the button as selected
   let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
+  event.target.classList.add(selectedColor);
   chrome.storage.sync.set({ color });
 }
 
-// Add a button to the segment for each supplied color
+// Add a button to the page for each supplied color
 function constructOptions(buttonColors) {
   chrome.storage.sync.get("color", (data) => {
     let currentColorSelection = data.color;
@@ -30,17 +30,17 @@ function constructOptions(buttonColors) {
       button.dataset.color = buttonColor;
       button.style.backgroundColor = buttonColor;
 
-      // …mark the currentColor selected color…
+      // …mark the currentColorly selected color…
       if (buttonColor === currentColorSelection) {
-        button.classList.add(selectedClassName);
+        button.classList.add(selectedColor);
       }
 
       // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
+      button.addEventListener("click", handleColorClick);
       colorSegment.appendChild(button);
     }
   });
 }
 
-// Initialize the segment by constructing the color options
+// Initialize the page by constructing the color options
 constructOptions(presetButtonColors);
