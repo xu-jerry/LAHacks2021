7b4d29e4af55e6800019d51d0705b39d88f80c35
xu-jerry <jerryxu4@gmail.com>
2021-03-24 17:00:55 -0700

    Now makes background darker

--- src/options.js
+++ src/options.js
@@ -1,5 +1,5 @@
 let page = document.getElementById("buttonDiv");
-let selectedClassName = "current";
+let selectedColor = "current";
 const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
 // Reacts to a button click by marking the selected button and saving
@@ -7,15 +7,15 @@ const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 function handleButtonClick(event) {
   // Remove styling from the previously selected color
   let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
+    `.${selectedColor}`
   );
   if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
+    current.classList.remove(selectedColor);
   }
 
   // Mark the button as selected
   let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
+  event.target.classList.add(selectedColor);
   chrome.storage.sync.set({ color });
 }
 
@@ -32,7 +32,7 @@ function constructOptions(buttonColors) {
 
       // …mark the currently selected color…
       if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
+        button.classList.add(selectedColor);
       }
 
       // …and register a listener for when that button is clicked
--- src/popup.js
+++ src/popup.js
@@ -40,23 +40,12 @@ remove.addEventListener("click", async () => {
     });
 });
 
-/*
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-    });
-}
-*/
-
 function refreshPage() {
     chrome.storage.sync.get("active", ({ active }) => {
         // different cases for whether we want script to be active
         if (active) {
-            chrome.storage.sync.get("color", ({ color }) => {
-                document.body.style.backgroundColor = color;
+            chrome.storage.sync.get("prevColor", ({ prevColor }) => {
+                document.body.style.backgroundColor = RGBToHex(hexToRGB(prevColor));
                 });
         }
         else {
@@ -65,6 +54,55 @@ function refreshPage() {
                 });
         }
     });
+
+    // helper functions for color manipulation
+    function RGBToHex(rgb) {
+        // Choose correct separator
+        let sep = rgb.indexOf(",") > -1 ? "," : " ";
+        // Turn "rgb(r,g,b)" into [r,g,b]
+        rgb = rgb.substr(4).split(")")[0].split(sep);
+        console.log(rgb[0]);
+        console.log(rgb[1]);
+        console.log(rgb[2]);
+
+        // changing colors HERE
+        let diff = 100;
+        rgb[0] = Math.max(0, rgb[0] - diff);
+        rgb[1] = Math.max(0, rgb[1] - diff);
+        rgb[2] = Math.max(0, rgb[2] - diff);
+
+        let r = (+rgb[0]).toString(16),
+            g = (+rgb[1]).toString(16),
+            b = (+rgb[2]).toString(16);
+        
+
+        if (r.length == 1)
+            r = "0" + r;
+        if (g.length == 1)
+            g = "0" + g;
+        if (b.length == 1)
+            b = "0" + b;
+        
+        return "#" + r + g + b;
+    }
+
+    function hexToRGB(h) {
+        let r = 0, g = 0, b = 0;
+
+        // 3 digits
+        if (h.length == 4) {
+          r = "0x" + h[1] + h[1];
+          g = "0x" + h[2] + h[2];
+          b = "0x" + h[3] + h[3];
+      
+        // 6 digits
+        } else if (h.length == 7) {
+          r = "0x" + h[1] + h[2];
+          g = "0x" + h[3] + h[4];
+          b = "0x" + h[5] + h[6];
+        }
+        return "rgb("+ +r + "," + +g + "," + +b + ")";
+    }
 }
 
 function resetCSS() {
@@ -84,8 +122,18 @@ function resetCSS() {
 }
 
 function setup() {
+    chrome.storage.sync.get("setup", (setup)=>{
+        if (setup === true) {
+            return;
+        }
+    });
     let prevColor = document.body.style.backgroundColor;
+    if (prevColor === undefined || prevColor === "") {
+        prevColor = "#FFFFFF";
+    }
     chrome.storage.sync.set({ prevColor });
     let active = false;
     chrome.storage.sync.set({ active });
+    let setup = true;
+    chrome.storage.sync.set({ setup });
 }
